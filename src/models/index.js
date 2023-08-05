b8618d8c4a48261c77d057ef31d2e6cc4549549f
millos910 <emontero910@gmail.com>
2023-08-04 21:23:36 -0500

    corregido

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,6 @@ const Actor = require("./Actor");
 const Director = require("./Director");
 const Genre=require("./Genre");
 const Movie = require("./Movie");
-Movie.belongsToMany(Genre,{through:"GenreMovies"})
-Movie.belongsToMany(Director,{through:"DirectorMovies"})
-Movie.belongsToMany(Actor,{through:"ActorsMovies"})
+Movie.belongsToMany(Genre,)//{through:"GenreMovies"})
+Movie.belongsToMany(Director,)//{through:"DirectorMovies"})
+Movie.belongsToMany(Actor,)//{through:"ActorsMovies"})
